25df6c7c9ef81a3cfa0d810c8b91294fb422f06b
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-03-05 16:16:12 +0100

    aggiunto programma calcolo del totale da pagare

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// dichiarazione delle variabili
+var burgerName = document.getElementById("burger-name");
+var ingredients = document.getElementsByClassName("add-ingredient");
+var calculate = document.getElementById("calculate");
+var sum = 50;
+var coupons = ["peppe20", "burgertime", "toohungry", "sconto"];
+var coupon = document.getElementById("coupon");
+var total = document.getElementById("total");
+//1. evento al click bottone calculate
+calculate.addEventListener("click",
+  function() {
+    // 2. inserimento nome panino
+    if ( burgerName.value != "" ){
+      // 3. controllo ingredienti inseriti e calcolo somma
+      for (var i = 0; i < ingredients.length; i++){
+        if (ingredients[i].checked == true){
+          sum += parseInt(ingredients[i].value);
+        }
+      }
+      // 4. calcolo dell'eventuale sconto
+      if (coupons.includes(coupon.value) == true){
+        sum -= sum * 0.2;
+      }
+      // 5. stampo il totale da pagare
+      total.innerHTML = sum.toFixed(2) +"€";
+    } else{
+      alert("Inserisci nome panino");
+    }
+  }
+);
