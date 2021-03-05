b7ee406a9b8b9f13270810e3d171d1452df11bb9
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-03-05 18:23:20 +0100

    fix somma continua ad ogni click button calculate

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@
 var burgerName = document.getElementById("burger-name");
 var ingredients = document.getElementsByClassName("add-ingredient");
 var calculate = document.getElementById("calculate");
-var sum = 50;
 var coupons = ["peppe20", "burgertime", "toohungry", "sconto"];
 var coupon = document.getElementById("coupon");
 var total = document.getElementById("total");
@@ -12,6 +11,7 @@ calculate.addEventListener("click",
     // 2. inserimento nome panino
     if ( burgerName.value != "" ){
       // 3. controllo ingredienti inseriti e calcolo somma
+      var sum = 50;
       for (var i = 0; i < ingredients.length; i++){
         if (ingredients[i].checked == true){
           sum += parseInt(ingredients[i].value);
